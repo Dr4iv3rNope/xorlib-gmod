0f95cb4e3595d67373beb20ba94991596a36320e
Dr4iv3rNope <dr4iv3r2004@gmail.com>
2022-11-21 00:08:53 +0300

    Added EachKey and EachValue

--- lua/xorlib/table/sh_enumeration.lua
+++ lua/xorlib/table/sh_enumeration.lua
@@ -8,6 +8,18 @@ end
 
 function x.EachPairs(tbl, callback)
 	for k, v in pairs(tbl) do
+		callback(k, v)
+	end
+end
+
+function x.EachKey(tbl, callback)
+	for k, v in pairs(tbl) do
+		callback(k)
+	end
+end
+
+function x.EachValue(tbl, callback)
+	for k, v in pairs(tbl) do
 		callback(v)
 	end
 end
