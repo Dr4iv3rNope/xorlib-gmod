9747b325e0245469cc5c016fb81553240fb37b72
Dr4iv3rNope <dr4iv3r2004@gmail.com>
2022-11-21 04:24:19 +0300

    Added x.DrawBlur function

---
+++ lua/xorlib/ui/cl_blur.lua
@@ -0,0 +1,21 @@
+local BLUR_MATERIAL = Material("pp/blurscreen")
+
+x.SetupScreenSize(function(w, h)
+	function x.DrawBlur(panel, power, passes)
+		power = power or 3
+		passes = passes or 3
+
+		local x, y = panel:LocalToScreen(0, 0)
+
+		surface.SetDrawColor(x.ColorWhite)
+		surface.SetMaterial(BLUR_MATERIAL)
+
+		for i = 1, passes do
+			BLUR_MATERIAL:SetFloat("$blur", (i / passes) * power)
+			BLUR_MATERIAL:Recompute()
+
+			render.UpdateScreenEffectTexture()
+			surface.DrawTexturedRect(-x, -y, w, h)
+		end
+	end
+end)
