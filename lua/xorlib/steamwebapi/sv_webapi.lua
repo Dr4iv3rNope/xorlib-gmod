b13893f595aeb22a16934745deb6504eea33ba47
Dr4iv3rNope <dr4iv3r2004@gmail.com>
2023-01-09 14:25:20 +0300

    Added steam web api library

---
+++ lua/xorlib/steamwebapi/sv_webapi.lua
@@ -0,0 +1,74 @@
+if not x.RequireModule("chttp") then return end
+
+x.STEAM_WEB_API_INTERFACE = x.STEAM_WEB_API_INTERFACE or {}
+x.STEAM_WEB_API_INTERFACE.__index = x.STEAM_WEB_API_INTERFACE
+
+function x.STEAM_WEB_API_INTERFACE:Request(interface, method, version, query, callback)
+	x.ExpectString(interface)
+	x.ExpectString(method)
+	x.ExpectNumber(version)
+	x.ExpectTableOrDefault(query, {})
+	x.ExpectFunctionOrDefault(callback)
+
+	query.key = self.Key
+
+	CHTTP({
+		url = string.format(
+			"https://api.steampowered.com/%s/%s/v%04d",
+			interface,
+			method,
+			version
+		),
+
+		parameters = query,
+
+		success = function(code, body)
+			local validationError
+			local json = util.JSONToTable(body)
+
+			if json and json.response then
+				json = json.response
+			else
+				x.Warn(
+					"[Steam Web API] Request %s/%s/%04d was success (%d), but no \"response\" key is available:\nBody: %s",
+					interface,
+					method,
+					version,
+					code,
+					body
+				)
+
+				validationError = "No \"response\" key"
+				json = nil
+			end
+
+			if callback then
+				callback(validationError, json)
+			end
+		end,
+
+		failed = function(code, body)
+			x.Warn(
+				"[Steam Web API] Request %s/%s/%04d failed (%d):\nBody: %s",
+				interface,
+				method,
+				version,
+				code,
+				tostring(body)
+			)
+
+			if callback then
+				callback(body, code)
+			end
+		end
+	})
+end
+
+function x.SteamWebApiInterface(key)
+	key = x.ExpectStringOrDefault(key, x._SteamWebApiKey)
+	x.Assert(key, "No steam web api key available!")
+
+	return setmetatable({
+		Key = key
+	}, x.STEAM_WEB_API_INTERFACE)
+end
