409759ea4f0e9e3c7d9dc82eb366a4923d21d9ac
Dr4iv3rNope <dr4iv3r2004@gmail.com>
2023-01-14 20:41:57 +0300

    Preparing for util.IsBinaryModuleInstalled

--- lua/xorlib/functional/sv_binary_modules.lua
+++ lua/xorlib/functional/sv_binary_modules.lua
@@ -1,32 +1,41 @@
-do
-	local isX86 = jit.arch == "x86"
+-- TODO: remove x.HasBinaryModule
+--
+-- util.IsBinaryModuleInstalled available on the dev branch 2022.08.12
+if util.IsBinaryModuleInstalled == x.HasBinaryModule then
+	do
+		local isX86 = jit.arch == "x86"
 
-	if system.IsLinux() then
-		x.BinaryArch = isX86 and "linux" or "linux64"
-	elseif system.IsWindows() then
-		x.BinaryArch = isX86 and "win32" or "win64"
-	elseif system.IsOSX() then
-		x.BinaryArch = "osx"
+		if system.IsLinux() then
+			x.BinaryArch = isX86 and "linux" or "linux64"
+		elseif system.IsWindows() then
+			x.BinaryArch = isX86 and "win32" or "win64"
+		elseif system.IsOSX() then
+			x.BinaryArch = "osx"
+		end
 	end
-end
 
-function x.HasBinaryModule(name)
-	return file.Exists(
-		string.format(
-			"bin/gmsv_%s_%s.dll",
-			name,
-			x.BinaryArch
-		),
-		"LUA"
-	)
+	function x.HasBinaryModule(name)
+		return file.Exists(
+			string.format(
+				"bin/gmsv_%s_%s.dll",
+				name,
+				x.BinaryArch
+			),
+			"LUA"
+		)
+	end
+
+	util.IsBinaryModuleInstalled = x.HasBinaryModule
+else
+	x.ErrorNoHalt("util.IsBinaryModuleInstalled has been added (2022.08.12)")
 end
 
 function x.RequireModule(name)
 	x.ExpectString(name)
 
-	if not x.HasBinaryModule(name) then
+	if not util.IsBinaryModuleInstalled(name) then
 		x.ErrorNoHalt(
-			"Binary module \"%s\" doesn't exist",
+			"Binary module \"%s\" not installed",
 			name
 		)
 
