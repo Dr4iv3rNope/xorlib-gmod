8987db42ee0c08b84af078223e67967ac284513c
Dr4iv3rNope <dr4iv3r2004@gmail.com>
2023-01-26 21:21:57 +0300

    Added x.PairsHasKey

--- lua/xorlib/table/sh_find.lua
+++ lua/xorlib/table/sh_find.lua
@@ -10,6 +10,16 @@ function x.SequenceHasValue(tbl, value)
 	return false
 end
 
+function x.PairsHasKey(tbl, value)
+	for k, v in pairs(tbl) do
+		if k == value then
+			return true
+		end
+	end
+
+	return false
+end
+
 function x.PairsHasValue(tbl, value)
 	for k, v in pairs(tbl) do
 		if v == value then
