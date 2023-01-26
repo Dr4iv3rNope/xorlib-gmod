3df6cd8ee3bfe8105c4c5ee9dd52f8ec0f6d8feb
Dr4iv3rNope <dr4iv3r2004@gmail.com>
2023-01-26 20:32:37 +0300

    Removed x.HasBinaryModule

--- lua/xorlib/functional/sv_binary_modules.lua
+++ lua/xorlib/functional/sv_binary_modules.lua
@@ -1,35 +1,3 @@
--- TODO: remove x.HasBinaryModule
---
--- util.IsBinaryModuleInstalled available on the dev branch 2022.08.12
-if util.IsBinaryModuleInstalled == x.HasBinaryModule then
-	do
-		local isX86 = jit.arch == "x86"
-
-		if system.IsLinux() then
-			x.BinaryArch = isX86 and "linux" or "linux64"
-		elseif system.IsWindows() then
-			x.BinaryArch = isX86 and "win32" or "win64"
-		elseif system.IsOSX() then
-			x.BinaryArch = "osx"
-		end
-	end
-
-	function x.HasBinaryModule(name)
-		return file.Exists(
-			string.format(
-				"bin/gmsv_%s_%s.dll",
-				name,
-				x.BinaryArch
-			),
-			"LUA"
-		)
-	end
-
-	util.IsBinaryModuleInstalled = x.HasBinaryModule
-else
-	x.ErrorNoHalt("util.IsBinaryModuleInstalled has been added (2022.08.12)")
-end
-
 function x.RequireModule(name)
 	x.ExpectString(name)
 
