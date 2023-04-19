ce31b09e264de09a0979c9b219515d78e8ffc98b
Dr4iv3rNope <dr4iv3r2004@gmail.com>
2023-04-19 16:48:19 +0300

    Using "MaskedPanel" for "CircleAvatarImage"

--- lua/xorlib/ui/cl_circle_avatar.lua
+++ lua/xorlib/ui/cl_circle_avatar.lua
@@ -3,28 +3,26 @@
 -- Three's Derma Library: https://github.com/Threebow/tdlib
 --
 
-xorlib.Dependency("xorlib/functional", "sh_getters.lua") -- x.Meta
+xorlib.Dependency("xorlib/ui", "cl_masked_panel.lua")
+xorlib.Dependency("xorlib/ui", "cl_poly.lua")
 
-local STENCILOPERATION_REPLACE			= STENCILOPERATION_REPLACE
-local STENCILOPERATION_ZERO				= STENCILOPERATION_ZERO
-local STENCILCOMPARISONFUNCTION_NEVER	= STENCILCOMPARISONFUNCTION_NEVER
-local STENCILCOMPARISONFUNCTION_EQUAL	= STENCILCOMPARISONFUNCTION_EQUAL
-local draw_NoTexture					= draw.NoTexture
-local surface_SetDrawColor				= surface.SetDrawColor
-local surface_DrawPoly					= surface.DrawPoly
-local render_ClearStencil				= render.ClearStencil
-local render_SetStencilEnable			= render.SetStencilEnable
-local render_SetStencilWriteMask		= render.SetStencilWriteMask
-local render_SetStencilTestMask			= render.SetStencilTestMask
-local render_SetStencilFailOperation	= render.SetStencilFailOperation
-local render_SetStencilPassOperation	= render.SetStencilPassOperation
-local render_SetStencilZFailOperation	= render.SetStencilZFailOperation
-local render_SetStencilCompareFunction	= render.SetStencilCompareFunction
-local render_SetStencilReferenceValue	= render.SetStencilReferenceValue
-local Panel_SetPaintManually			= x.Meta("Panel", "SetPaintedManually")
-local Panel_PaintManual					= x.Meta("Panel", "PaintManual")
+local MaskedPanel = baseclass.Get("MaskedPanel")
 
 local PANEL = {}
+
+function PANEL:Init()
+	MaskedPanel.Init(self)
+
+	self._Avatar = vgui.Create("AvatarImage", self)
+end
+
+function PANEL:BuildMask(w, h)
+	return x.PolyCircle(w / 2, h / 2, 1, 32, w / 2, 0)
+end
+
+function PANEL:PerformLayout(w, h)
+	self.Avatar:SetSize(w, h)
+end
 
 local function alias(funcName)
 	PANEL[funcName] = function(self, ...)
@@ -34,52 +32,7 @@ local function alias(funcName)
 	end
 end
 
-function PANEL:Init()
-	self.Avatar = vgui.Create("AvatarImage", self)
-	self.CircleMask = {}
-end
-
-function PANEL:PerformLayout(w, h)
-	self.CircleMask = x.PolyCircle(w / 2, h / 2, 1, 32, w / 2, 0)
-
-	self.Avatar:SetSize(w, h)
-end
-
-function PANEL:Paint(w, h)
-	-- https://github.com/Threebow/tdlib/blob/ee8f562ab5d1f2540491ded39990843da0d9fb00/tdlib.lua#L415-L444
-	render_ClearStencil()
-	render_SetStencilEnable(true)
-
-	render_SetStencilWriteMask(1)
-	render_SetStencilTestMask(1)
-
-	render_SetStencilFailOperation(STENCILOPERATION_REPLACE)
-	render_SetStencilPassOperation(STENCILOPERATION_ZERO)
-	render_SetStencilZFailOperation(STENCILOPERATION_ZERO)
-	render_SetStencilCompareFunction(STENCILCOMPARISONFUNCTION_NEVER)
-	render_SetStencilReferenceValue(1)
-
-	draw_NoTexture()
-	surface_SetDrawColor(255, 255, 255)
-	surface_DrawPoly(self.CircleMask)
-
-	render_SetStencilFailOperation(STENCILOPERATION_ZERO)
-	render_SetStencilPassOperation(STENCILOPERATION_REPLACE)
-	render_SetStencilZFailOperation(STENCILOPERATION_ZERO)
-	render_SetStencilCompareFunction(STENCILCOMPARISONFUNCTION_EQUAL)
-	render_SetStencilReferenceValue(1)
-
-	local avatar = self.Avatar
-
-	Panel_SetPaintManually(avatar, false)
-	Panel_PaintManual(avatar)
-	Panel_SetPaintManually(avatar, true)
-
-	render_SetStencilEnable(false)
-	render_ClearStencil()
-end
-
 alias("SetPlayer")
 alias("SetSteamID")
 
-vgui.Register("CircleAvatarImage", PANEL, "Panel")
+vgui.Register("CircleAvatarImage", PANEL, "MaskedPanel")
