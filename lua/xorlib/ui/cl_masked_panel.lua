97aeff4145dffc734d42408bcbfc326beb09e8a1
Dr4iv3rNope <dr4iv3r2004@gmail.com>
2023-04-19 16:48:04 +0300

    Added "MaskedPanel" panel

---
+++ lua/xorlib/ui/cl_masked_panel.lua
@@ -0,0 +1,40 @@
+xorlib.Dependency("xorlib/ui", "cl_mask.lua")
+
+local StartDrawMasked	= x.StartDrawMasked
+local EndDrawMasked		= x.EndDrawMasked
+
+local PANEL = {}
+
+function PANEL:Init()
+	self._CachedMask = {}
+end
+
+function PANEL:PerformLayout(w, h)
+	self._CachedMask = self:BuildMask(w, h)
+end
+
+function PANEL:Paint(w, h)
+	StartDrawMasked(self._CachedMask)
+
+	for _, child in ipairs(self:GetChildren()) do
+		child:SetPaintManually(true)
+		child:PaintManual()
+		child:SetPaintManually(false)
+	end
+
+	self:PaintMasked(w, h)
+
+	EndDrawMasked()
+end
+
+function PANEL:BuildMask(w, h)
+	-- for override
+
+	return {}
+end
+
+function PANEL:PaintMasked(w, h)
+	-- for override
+end
+
+vgui.Register("MaskedPanel", PANEL, "Panel")
