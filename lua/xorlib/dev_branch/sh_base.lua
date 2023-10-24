41533680b2a94e8b20e7c05f8cd03dfbaffae528
Dr4iv3rNope <dr4iv3r2004@gmail.com>
2023-10-24 03:23:21 +0300

    Add x.CheckDevBranchFunction

---
+++ lua/xorlib/dev_branch/sh_base.lua
@@ -0,0 +1,16 @@
+xorlib.Dependency("xorlib/console",    "sh_print.lua")
+xorlib.Dependency("xorlib/functional", "sh_getters.lua")
+
+function xorlib.CheckDevBranchFunction(functionPath)
+    local functionToCheck = x.IndexGlobalFunction(functionPath)
+
+    if functionToCheck ~= nil then
+        x.ErrorNoHalt("Dev branch function \"%s\" is available! " ..
+                      "Remove temporary implementaion!",
+                      functionPath)
+
+        return true
+    end
+
+    return false
+end
