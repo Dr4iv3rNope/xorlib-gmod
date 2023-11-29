9376aeb616826ce6437292f44ab05aa4155c4c21
Dr4iv3rNope <dr4iv3r2004@gmail.com>
2023-11-29 03:25:03 +0300

    Fix error

--- lua/xorlib/dev_branch/sh_base.lua
+++ lua/xorlib/dev_branch/sh_base.lua
@@ -2,7 +2,7 @@ xorlib.Dependency("xorlib/console",    "sh_print.lua")
 xorlib.Dependency("xorlib/functional", "sh_getters.lua")
 
 function xorlib.CheckDevBranchFunction(functionPath)
-    local functionToCheck = x.IndexGlobalFunction(functionPath, true)
+    local functionToCheck = x.Index(functionPath, true)
 
     if functionToCheck ~= nil then
         x.ErrorNoHalt("Dev branch function \"%s\" is available! " ..
