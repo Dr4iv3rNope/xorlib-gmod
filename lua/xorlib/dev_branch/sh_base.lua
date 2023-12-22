0e1652f6e11db3ee59f03cbd4699b71227d5933b
Dr4iv3rNope <dr4iv3r2004@gmail.com>
2023-12-22 22:00:38 +0300

    Add "entry" argument to x.CheckDevBranchFunction

--- lua/xorlib/dev_branch/sh_base.lua
+++ lua/xorlib/dev_branch/sh_base.lua
@@ -1,8 +1,8 @@
 xorlib.Dependency("xorlib/console",    "sh_print.lua")
 xorlib.Dependency("xorlib/functional", "sh_getters.lua")
 
-function xorlib.CheckDevBranchFunction(functionPath)
-    local functionToCheck = x.Index(nil, functionPath)
+function xorlib.CheckDevBranchFunction(entry, functionPath)
+    local functionToCheck = x.Index(entry, functionPath)
 
     if functionToCheck ~= nil then
         x.ErrorNoHalt("Dev branch function \"%s\" is available! " ..
--- lua/xorlib/dev_branch/sh_changes.lua
+++ lua/xorlib/dev_branch/sh_changes.lua
@@ -1,20 +1,20 @@
 xorlib.Dependency("xorlib/dev_branch", "sh_base.lua")
 
-if not xorlib.CheckDevBranchFunction("net.ReadPlayer") then
+if not xorlib.CheckDevBranchFunction(nil, "net.ReadPlayer") then
     net.ReadPlayer = net.ReadEntity
 end
 
-if not xorlib.CheckDevBranchFunction("net.WritePlayer") then
+if not xorlib.CheckDevBranchFunction(nil, "net.WritePlayer") then
     net.WritePlayer = net.WriteEntity
 end
 
-if not xorlib.CheckDevBranchFunction("ents.Iterator") then
+if not xorlib.CheckDevBranchFunction(nil, "ents.Iterator") then
     function ents.Iterator()
         return ipairs(ents.GetAll())
     end
 end
 
-if not xorlib.CheckDevBranchFunction("player.Iterator") then
+if not xorlib.CheckDevBranchFunction(nil, "player.Iterator") then
     function player.Iterator()
         return ipairs(player.GetAll())
     end
