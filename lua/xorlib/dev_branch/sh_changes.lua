d47ee50f8ed9ce15dae6b08977fa6554a10753cc
Dr4iv3rNope <dr4iv3r2004@gmail.com>
2023-12-22 22:01:00 +0300

    Implement dev branch function PLAYER:IsWalking

--- lua/xorlib/dev_branch/sh_changes.lua
+++ lua/xorlib/dev_branch/sh_changes.lua
@@ -1,5 +1,7 @@
 xorlib.Dependency("xorlib/dev_branch", "sh_base.lua")
 
+local PLAYER = FindMetaTable("Player")
+
 if not xorlib.CheckDevBranchFunction(nil, "net.ReadPlayer") then
     net.ReadPlayer = net.ReadEntity
 end
@@ -17,5 +19,11 @@ end
 if not xorlib.CheckDevBranchFunction(nil, "player.Iterator") then
     function player.Iterator()
         return ipairs(player.GetAll())
+    end
+end
+
+if not xorlib.CheckDevBranchFunction(PLAYER, "IsWalking") then
+    function PLAYER:IsWalking()
+        return self:KeyDown(IN_WALK)
     end
 end
