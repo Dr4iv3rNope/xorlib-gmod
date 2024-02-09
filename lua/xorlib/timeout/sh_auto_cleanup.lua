0dd07d7b49505193e0bf5f72f7d40556b6ee371d
Dr4iv3rNope <dr4iv3r2004@gmail.com>
2024-02-09 17:14:02 +0300

    Fix error
    [xorlib] addons/xorlib/lua/xorlib/timeout/sh_auto_cleanup.lua:11: attempt to compare string with number
      1. callback - addons/xorlib/lua/xorlib/timeout/sh_auto_cleanup.lua:11
       2. FilterPairs - addons/xorlib/lua/xorlib/table/sh_filter.lua:36
        3. unknown - addons/xorlib/lua/xorlib/timeout/sh_auto_cleanup.lua:10

--- lua/xorlib/timeout/sh_auto_cleanup.lua
+++ lua/xorlib/timeout/sh_auto_cleanup.lua
@@ -7,7 +7,7 @@ local globalTimeoutTable = x.TimeoutTable
 timer.Create("xorlib_timeout_cleanup", 60, 0, function()
     local t = CurTime()
 
-    x.FilterPairs(globalTimeoutTable, function(nextCheck)
+    x.FilterValues(globalTimeoutTable, function(nextCheck)
         return t > nextCheck
     end)
 end)
