552021bcd90850463e8e30a28c1fafabe29d689f
Dr4iv3rNope <dr4iv3r2004@gmail.com>
2024-06-25 23:16:02 +0300

    Add russian language

---
+++ lua/xorlib_languages/sh_ru.lua
@@ -0,0 +1,7 @@
+local LANG = x.XorlibLanguageContext:DefineLanguage("en")
+
+LANG:Phrase("time_formatter.seconds", "сек.")
+LANG:Phrase("time_formatter.minutes", "м.")
+LANG:Phrase("time_formatter.hours",   "ч.")
+LANG:Phrase("time_formatter.days",    "д.")
+LANG:Phrase("time_formatter.weeks",   "н.")
