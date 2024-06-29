0ff20a4cf05fdea3a87e9bdac3b9a7782a1f09c4
Dr4iv3rNope <dr4iv3r2004@gmail.com>
2024-06-29 04:22:26 +0300

    Properly clear x.*IDPlayers tables

--- lua/xorlib/player/sh_id_getters.lua
+++ lua/xorlib/player/sh_id_getters.lua
@@ -5,10 +5,20 @@ x._Old_player_GetBySteamID   = x._Old_player_GetBySteamID   or player.GetBySteam
 x._Old_player_GetBySteamID64 = x._Old_player_GetBySteamID64 or player.GetBySteamID64
 x._Old_player_GetByAccountID = x._Old_player_GetByAccountID or player.GetByAccountID
 
+--[[
+    x.PlayerIDCache[userid] = {
+        SteamID = string,
+        SteamID64 = string,
+        AccountID = number,
+    }
+]]
+x.PlayerIDCache = x.PlayerIDCache or {}
+
 x.SteamIDPlayers             = x.SteamIDPlayers or {}
 x.SteamID64Players           = x.SteamID64Players or {}
 x.AccountIDPlayers           = x.AccountIDPlayers or {}
 
+local playerIdCache          = x.PlayerIDCache
 local steamIDPlayers         = x.SteamIDPlayers
 local steamID64Players       = x.SteamID64Players
 local accountIDPlayers       = x.AccountIDPlayers
@@ -30,21 +40,35 @@ end
 local function addPlayer(ply)
     if ply:IsBot() then return end
 
+    playerIdCache[ply:UserID()] = {
+        SteamID   = ply:SteamID(),
+        SteamID64 = ply:SteamID64(),
+        AccountID = ply:AccountID()
+    }
+
     steamIDPlayers[ply:SteamID()]     = ply
     steamID64Players[ply:SteamID64()] = ply
     accountIDPlayers[ply:AccountID()] = ply
 end
 
-local function removePlayer(ply)
-    if ply:IsBot() then return end
+local function removePlayerByUserID(userId)
+    local idCache = x.PlayerIDCache[userId]
 
-    steamIDPlayers[ply:SteamID()]     = nil
-    steamID64Players[ply:SteamID64()] = nil
-    accountIDPlayers[ply:AccountID()] = nil
+    x.PlayerIDCache[userId] = nil
+
+    steamIDPlayers[idCache.SteamID]     = nil
+    steamID64Players[idCache.SteamID64] = nil
+    accountIDPlayers[idCache.AccountID] = nil
 end
 
 hook.Add(SERVER and "PlayerInitialSpawn" or "CL_PlayerInitialSpawn", "xorlib_id_getters", addPlayer, HOOK_MONITOR_HIGH)
-hook.Add(SERVER and "PlayerDisconnected" or "CL_PlayerDisconnected", "xorlib_id_getters", removePlayer, HOOK_MONITOR_LOW)
+
+gameevent.Listen("player_disconnect")
+hook.Add("player_disconnect", "xorlib_id_getters", function(data)
+    if data.bot == 1 then return end
+
+    removePlayerByUserID(data.userid)
+end, HOOK_MONITOR_LOW)
 
 x.EnsureInitPostEntity(function()
     x.EachSequence(player.GetAll(), addPlayer)
