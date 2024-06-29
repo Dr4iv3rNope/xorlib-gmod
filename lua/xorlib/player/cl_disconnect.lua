1f9d92e30864e9bbdd0199c62970ff51332a3ff8
Dr4iv3rNope <dr4iv3r2004@gmail.com>
2024-06-29 04:21:51 +0300

    Fix CL_PlayerDisconnected hook

--- lua/xorlib/player/cl_disconnect.lua
+++ lua/xorlib/player/cl_disconnect.lua
@@ -1,9 +1,16 @@
 xorlib.Dependency("xorlib/hook", "sh_hooks.lua") -- HOOK_*
 
-gameevent.Listen("player_disconnect")
+x.UserIDPlayerInstanceCache = x.UserIDPlayerInstanceCache or {}
 
-hook.Add("player_disconnect", "xorlib_id_getters", function(data)
-    local ply = Player(data.userid)
+hook.Add("CL_PlayerInitialSpawn", "xorlib_cl_disconnect", function(ply)
+    x.UserIDPlayerInstanceCache[ply:UserID()] = ply
+end, HOOK_MONITOR_HIGH)
+
+gameevent.Listen("player_disconnect")
+hook.Add("player_disconnect", "xorlib_cl_disconnect", function(data)
+    local ply = x.UserIDPlayerInstanceCache[data.userid]
+
+    x.UserIDPlayerInstanceCache[data.userid] = nil
 
     hook.Run("CL_PlayerDisconnected", ply)
 end, HOOK_MONITOR_LOW)
